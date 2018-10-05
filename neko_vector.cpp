e4d52351f6f3de2f3a04a267b1bf92dcb8b5bf78
Prilog <31861490+Prilog@users.noreply.github.com>
2018-10-05 21:39:59 +0300

    HW3.2

--- neko_vector.cpp
+++ neko_vector.cpp
@@ -7,6 +7,7 @@ typedef unsigned long long ull;
 neko_vector::neko_vector() {
 	buf = 0;
 	sz = 0;
+	own = true;
 	arr.reset();
 }
 
@@ -14,6 +15,15 @@ neko_vector::~neko_vector() {
 	if (arr) {
 		arr.reset();
 	}
+}
+
+void neko_vector::copy_vector() {
+	if (own) {
+		return;
+	}
+	own = true;
+	std::vector <ui> x = *arr;
+	arr = std::make_shared <std::vector <ui> >(x);
 }
 
 void neko_vector::create_vector() {
@@ -30,9 +40,11 @@ neko_vector::neko_vector(neko_vector const &arg) {
 	sz = arg.sz;
 	if (arg.sz > 1) {
 		buf = 0;
+		own = false;
 		arr = arg.arr;
 	}
 	else {
+		own = true;
 		buf = arg.buf;;
 		delete_vector();
 	}
@@ -47,6 +59,7 @@ void neko_vector::push_back(const ui arg) {
 		arr->push_back(arg);
 	}
 	else {
+		copy_vector();
 		arr->push_back(arg);
 	}
 	sz++;
@@ -58,9 +71,11 @@ void neko_vector::pop_back() {
 	}
 	else if (sz == 2) {
 		buf = (*arr)[0];
+		own = true;
 		delete_vector();
 	}
 	else {
+		copy_vector();
 		arr->pop_back();
 	}
 	sz--;
@@ -84,6 +99,7 @@ ui& neko_vector::operator[](size_t index) {
 		return buf;
 	}
 	else {
+		copy_vector();
 		return (*arr)[index];
 	}
 }
@@ -93,6 +109,7 @@ neko_vector& neko_vector::operator=(neko_vector const &arg) {
 		buf = arg.buf;
 	}
 	else {
+		copy_vector();
 		sz = arg.sz;
 		buf = 0;
 		arr = arg.arr;
@@ -103,5 +120,6 @@ neko_vector& neko_vector::operator=(neko_vector const &arg) {
 void neko_vector::clear() {
 	sz = 0;
 	buf = 0;
+	own = true;
 	delete_vector();
 }
